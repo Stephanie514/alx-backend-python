6d32ecc268e24bb9e0ec1fe7a337aa467b92b27b
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-20 15:04:09 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,15 +2,14 @@
 """Task 12 module.
 """
 
-from typing import Tuple
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    """Using mypy to validate the code"""
-    zoomed_in: Tuple = tuple(
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
-    )
+    ]
     return zoomed_in
 
 
