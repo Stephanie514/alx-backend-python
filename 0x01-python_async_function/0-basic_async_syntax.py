bc736244993ab6052e35d98eb95e092f7b8f4656
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-22 18:06:52 +0000

    Let's execute multiple coroutines at the same time with async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,16 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """asynchronous coroutine that takes in an integer
-    argument (max_delay"""
+    """
+    Asynchronous coroutine that takes in an integer argument (max_delay)
+    and waits for a random delay between 0 and max_delay (inclusive).
+
+    Parameters:
+        max_delay (int): Maximum delay value (default is 10).
+
+    Returns:
+        float: Random delay value.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
